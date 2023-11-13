2b20407dfb678aff6b9c9893a65d3621e97950a1
GuilhermeHalter <guisam.halter@gmail.com>
2023-11-13 16:35:53 -0300

    Feito passo 4: Formateção dos arquivos do projeto

--- Innovar/admin.py
+++ Innovar/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+
 from .models import Pacote, Procedimentos
 
 admin.site.register(Pacote)
--- Innovar/migrations/0001_initial.py
+++ Innovar/migrations/0001_initial.py
@@ -4,19 +4,17 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='Pacote',
+            name="Pacote",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('nome', models.CharField(max_length=100)),
-                ('descricao', models.CharField(max_length=100)),
+                ("id", models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name="ID")),
+                ("nome", models.CharField(max_length=100)),
+                ("descricao", models.CharField(max_length=100)),
             ],
         ),
     ]
