69076e429258977027ab6836115ae78068328439
GuilhermeHalter <guisam.halter@gmail.com>
2023-11-13 16:28:24 -0300

    Feito até o passo 4

---
+++ Innovar/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Pacote, Procedimentos
+
+admin.site.register(Pacote)
+admin.site.register(Procedimentos)
---
+++ Innovar/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class Pacote(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+    
+class Procedimentos(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+    
+
+
