8cde2b3a79a47b7d32aa48c90171509c22a1b779
GuilhermeHalter <guisam.halter@gmail.com>
2023-12-04 15:08:31 -0300

    feito attachment_key in serrializers

--- Innovar/serializers/pacote.py
+++ Innovar/serializers/pacote.py
@@ -1,8 +1,20 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SlugRelatedField
+
+from uploader.models import Image
+from uploader.serializers import ImageSerializer
 
 from Innovar.models import Pacote
 
 class PacoteSerializer(ModelSerializer):
+    cover_attachment_key = SlugRelatedField(
+        source="imagem",
+        queryset=Image.objects.all(),
+        slug_field="attachment_key",
+        required=False,
+        write_only=True,
+    )
+    imagem = ImageSerializer(required=False, read_only=True)
+
     class Meta:
         model = Pacote
         fields = "__all__"
--- Innovar/serializers/procedimento.py
+++ Innovar/serializers/procedimento.py
@@ -1,8 +1,20 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SlugRelatedField
 
 from Innovar.models import Procedimentos
 
+from uploader.models import Image
+from uploader.serializers import ImageSerializer
+
 class ProcedimentoSerializer(ModelSerializer):
+    cover_attachment_key = SlugRelatedField(
+        source="imagem",
+        queryset=Image.objects.all(),
+        slug_field="attachment_key",
+        required=False,
+        write_only=True,
+    )
+    imagem = ImageSerializer(required=False, read_only=True)
+
     class Meta:
         model = Procedimentos
         fields = "__all__"
